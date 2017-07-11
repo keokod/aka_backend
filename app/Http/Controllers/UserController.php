f75fa9316271f39e5517e0f32c0c95cc8996be85
keokod <debian@localhost.domaine>
2017-07-11 06:26:46 +0200

    ajout de la récupération d'un membre avec sa compétence , dans le controller la méthode getUser on utilise la jointure sur la table compétence

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,22 +18,27 @@ class UserController extends Controller
         header("Access-Control-Allow-Origin: *");
     }
 
-    public function getUser()
+    public function getUser() 
     {
         $sql="select * from user";
-        $results = app('db')->select($sql);
 
+        $results = app('db')->select($sql);
 
         return json_encode($results);
     }
 
     public function getMembre($id)
     {
-        $sql="select * from user where user_id= $id";
-        //echo $sql;
+        $sql= "SELECT user.user_id as user_id, user.pseudo as pseudo, user.avatar as avatar, competences.nom as competence
+        FROM user
+        LEFT JOIN userCompetence
+        ON user.user_id = userCompetence.user_id
+        LEFT JOIN competences
+        ON userCompetence.user_id = competences.competence_id
+        WHERE user.user_id = $id";
+
         $results = app('db')->select($sql);
-        
         return json_encode($results);
     }
-    //
+
 }
